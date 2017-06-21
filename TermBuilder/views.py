33cdfb8e14569193a8a100eb9aebd0c6da3c4e3c
Lane <lane.hensley@hotmail.com>
2017-06-20 20:38:53 -0500

    created a base and homepage template and tested that homepage extends base

--- TermBuilder/views.py
+++ TermBuilder/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.template import loader
+from django.template.response import TemplateResponse
 
-# Create your views here.
+
+def home_page(request):
+	template = loader.get_template('home_page.html')
+	return TemplateResponse(request, template, {})
+
