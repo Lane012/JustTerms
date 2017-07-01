fffc9822da8f93c52214b4b3e17935237dcd2bfe
Lane <lane.hensley@hotmail.com>
2017-07-01 18:24:15 -0500

    add Word and definition

--- TermBuilder/models.py
+++ TermBuilder/models.py
@@ -2,10 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.db.models.fields.related import OneToOneField
+
+
+class Word(models.Model):
+    value = models.CharField(max_length=64)
+    
+class Definition(models.Model):
+    value = models.TextField(max_length=500)
+    word = OneToOneField(Word, on_delete=models.CASCADE)
+    
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    wordList = models.TextField(max_length=500)
+    words = models.ForeignKey(Word, on_delete=models.CASCADE, null=True)
     
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created,  **kwargs):
@@ -17,12 +27,17 @@ class Profile(models.Model):
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
     
-    def setWordList(self, wordList):
-        self.wordList = wordList
+    def setWordList(self, words, definitions):
+        self.words = words
+        self.definitions = definitions
         self.save()
     
     def getWordList(self):
-        return self.wordList
+        return (self.words, self.definitions)
+    
+ 
+    
+
     
     
         
